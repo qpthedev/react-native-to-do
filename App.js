d01e22279c8b10beffc933f5eb1d5caed0a6d81d
qpthedev <qpthedev@gmail.com>
2021-10-06 23:49:14 -0400

    Basic buttons and text input UI

--- App.js
+++ App.js
@@ -1,7 +1,70 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
-  return <View></View>;
+  const [text, setText] = useState("");
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Pressable onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </Pressable>
+
+        <Pressable onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </Pressable>
+      </View>
+
+      <TextInput
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    marginTop: 20,
+    fontSize: 18,
+  },
+});
