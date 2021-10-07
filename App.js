129ba3f8d54dff5262a0d9ec2bf909962822e521
qpthedev <qpthedev@gmail.com>
2021-10-07 03:37:24 -0400

    Impletment save, load, scrollview for to do list

--- App.js
+++ App.js
@@ -1,14 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [text, setText] = useState("");
   const [working, setWorking] = useState(true);
+  const [toDos, setToDos] = useState({});
+
+  // Load To Do when component mounts
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+
+  const saveToDos = async (toSave) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const loadToDos = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+      setToDos(JSON.parse(s));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
 
   return (
     <View style={styles.container}>
@@ -39,7 +87,19 @@ export default function App() {
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         onChangeText={onChangeText}
         value={text}
+        onSubmitEditing={addToDo}
+        returnKeyType="done"
       />
+
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View key={key} style={styles.toDo}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -64,7 +124,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 15,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "500",
   },
 });
